346055eb53e97740ced45b1af8cca14b6615cc72
Daniel Bingham <23138229+danieljbingham@users.noreply.github.com>
2025-02-07 16:37:06 +0000

    small refactor and todo

--- src/main/scala/schrodinger/CircuitBreaker.scala
+++ src/main/scala/schrodinger/CircuitBreaker.scala
@@ -29,28 +29,30 @@ object CircuitBreaker {
     for {
       ref <- Ref[F].of(Closed(0))
       clock = Clock[F]
-      cb = new CircuitBreaker[F] {
-        def protect[A](f: => F[A]): F[Either[Throwable, A]] = ref.get.flatMap {
-          case Closed(failures) =>
-            f.attempt.flatTap {
-              case Left(_) if failures + 1 >= maxFailures => clock.realTime.flatMap(time => ref.set(Open(time.toMillis)))
-              case Left(_)                                => ref.set(Closed(failures + 1))
-              case Right(_)                               => ().pure
-            }
-          case Open(startTime) =>
-            isElapsed(startTime, resetTimeout, clock).flatMap {
-              case true =>
-                ref.set(HalfOpen) *> f.attempt.flatTap {
-                  case Right(value) => ref.set(Closed(0))
-                  case Left(value)  => ref.set(Open(System.currentTimeMillis()))
-                }
-              case false => Left(CircuitBreakerOpenException).pure
-            }
-          // TODO: implement half-open state to define when to move back to closed, currently handled by Open state
-          case HalfOpen => Left(CircuitBreakerOpenException).pure
-        }
+    } yield new CircuitBreaker[F] {
+      def protect[A](f: => F[A]): F[Either[Throwable, A]] = ref.get.flatMap {
+        case Closed(failures) =>
+          f.attempt.flatTap {
+            case Left(_) if failures + 1 >= maxFailures => clock.realTime.flatMap(time => ref.set(Open(time.toMillis)))
+            case Left(_)                                => ref.set(Closed(failures + 1))
+            case Right(_)                               => ().pure
+          }
+        case Open(startTime) =>
+          isElapsed(startTime, resetTimeout, clock).flatMap {
+            case true =>
+              ref.set(HalfOpen) *> f.attempt.flatTap {
+                case Right(value) => ref.set(Closed(0))
+                case Left(value)  => ref.set(Open(System.currentTimeMillis()))
+              }
+            case false => Left(CircuitBreakerOpenException).pure
+          }
+        /*
+         * TODO: implement half-open state to define when to move back to closed based on proportion of successes or something,
+         * currently bypassed by Open state
+         */
+        case HalfOpen => Left(CircuitBreakerOpenException).pure
       }
-    } yield cb
+    }
 
   private def isElapsed[F[_]: Concurrent](startTime: Long, resetTimeout: FiniteDuration, clock: Clock[F]): F[Boolean] =
     clock.realTime.map(time => (time.toMillis - startTime) >= resetTimeout.toMillis)
